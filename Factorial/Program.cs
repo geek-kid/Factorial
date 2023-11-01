8c0a3564fc0fc4727fab77e5d57174e2c26b5031
Arya <aryatahery@gmail.com>
2023-11-01 08:41:52 +0330

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+int num;
+
+Console.WriteLine("your num?");
+try
+{
+    num = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Bad Input");
+    return;
+}
+Console.WriteLine($"result: {calcfac(num)}");
+Console.ReadKey();
+
+static long calcfac(int num) {
+    if (num == 0) return 1;
+    return num * calcfac(num - 1);
+}
